fc6520028bc516af3a50b86b84b1d8a62d228a20
Gabriel Castro <gabri.gcl@gmail.com>
2020-07-21 00:24:24 -0300

    Add list recipe feature

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -47,7 +47,7 @@ class AuthTokenSerializer(serializers.Serializer):
             password=password
         )
         if not user:
-            msg = _('Unable to authenticate with providade credentials')
+            msg = _('Unable to authenticate with provided credentials')
             raise serializers.ValidationError(msg, code='authentication')
         attrs['user'] = user
         return attrs
